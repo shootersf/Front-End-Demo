c4e839dffca80dbcf4d6867bb7922777795f616a
shootersf <me@simonfarrelly.com>
2018-07-20 14:36:44 +0100

    Active Nav link updates on scroll

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ let addSmoothAtStart = function() {
 
 /*EVENT HANDLERS*/
 
-//Watches for a point during scrolling to toggle the navbar look
+//Watches for a point during scrolling to toggle the navbar look and updates active link at all times
 window.onscroll = function() {
     const SHOULD_BE_DARK = 160;
 
@@ -27,6 +27,16 @@ window.onscroll = function() {
         toggleNav();
     else if(window.scrollY < SHOULD_BE_DARK && darkNavCheck())
         toggleNav();
+    
+    adjustActiveLinkNav(window.scrollY);
+}
+
+document.getElementById("learn-more").onclick = function() {
+    let aboutSection = document.getElementById("about");
+    aboutSection.scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+    })
 }
 
 //HELPER FUNCTIONS
@@ -40,3 +50,27 @@ function darkNavCheck() {
 function toggleNav() {
     document.getElementById("nav").classList.toggle("nav-dark");
 }
+
+function adjustActiveLinkNav(winScroll) {
+    let aboveScroll;
+
+    const sections = document.getElementsByTagName("section");
+    const links = document.getElementsByClassName("nav-link");
+    //find last section above the scroll
+    for (let i = 0; i < sections.length; i++)
+    {
+        console.log(sections[i].offsetTop, winScroll);
+        if (sections[i].offsetTop <= winScroll)
+            aboveScroll = sections[i];  //will keep being replaced until the last element above
+    }
+    console.log(aboveScroll);
+    //step through and adjust active link
+    for (let i = 0; i < links.length; i++)
+    {
+        if (aboveScroll === sections[i])
+            links[i].classList.add("active");
+        else
+            links[i].classList.remove("active");
+    }
+}
+
