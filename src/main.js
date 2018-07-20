c9c34fe34c5d7bb47d5b32c55d5de3e7a04dad1e
shootersf <me@simonfarrelly.com>
2018-07-20 13:44:13 +0100

    Dark Nav added

--- src/main.js
+++ src/main.js
@@ -1,14 +1,42 @@
-let anchorlinks = document.querySelectorAll('a[href^="#"]')
-console.log(anchorlinks);
-for (let item of anchorlinks) { // relitere 
-    item.addEventListener('click', (e)=> {
-        let hashval = item.getAttribute('href')
-        let target = document.querySelector(hashval)
-        target.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
+/*FUNCTIONS RAN ON START*/
+
+//Adds smooth scrolling to anchor links
+let addSmoothAtStart = function() {
+    let anchorlinks = document.querySelectorAll('a[href^="#"]')
+    for (let item of anchorlinks) {
+        item.addEventListener('click', (e)=> {
+            let hashval = item.getAttribute('href')
+            let target = document.querySelector(hashval)
+            target.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            })
+            history.pushState(null, null, hashval)
+            e.preventDefault()
         })
-        history.pushState(null, null, hashval)
-        e.preventDefault()
-    })
+    }
+}();
+
+/*EVENT HANDLERS*/
+
+//Watches for a point during scrolling to toggle the navbar look
+window.onscroll = function() {
+    const SHOULD_BE_DARK = 160;
+
+    if(window.scrollY >= SHOULD_BE_DARK && !darkNavCheck())
+        toggleNav();
+    else if(window.scrollY < SHOULD_BE_DARK && darkNavCheck())
+        toggleNav();
+}
+
+//HELPER FUNCTIONS
+
+function darkNavCheck() {
+    let nav = document.getElementById("nav");
+    return nav.classList.contains("nav-dark") ? true : false;
+        
+}
+
+function toggleNav() {
+    document.getElementById("nav").classList.toggle("nav-dark");
 }
